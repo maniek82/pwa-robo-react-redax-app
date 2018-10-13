8536d392be8e3860d4c85b46a5e22866b632887a
maniek82 <mariusz.tripler@gmail.com>
2018-10-13 11:10:54 +0100

    Updates

--- precache-manifest.94d465e3c65f59321fd34c8b51bde243.js
+++ precache-manifest.94d465e3c65f59321fd34c8b51bde243.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/pwa-robo-react-redax-app/static/js/runtime~main.7d3234be.js"
   },
   {
-    "revision": "b68a8f15c771d4b6727b",
-    "url": "/pwa-robo-react-redax-app/static/js/main.b68a8f15.chunk.js"
+    "revision": "0c69f2e16c3bc04e5f14",
+    "url": "/pwa-robo-react-redax-app/static/js/main.0c69f2e1.chunk.js"
   },
   {
     "revision": "f17fbf04b23e4bb0de2c",
     "url": "/pwa-robo-react-redax-app/static/js/1.f17fbf04.chunk.js"
   },
   {
-    "revision": "b68a8f15c771d4b6727b",
+    "revision": "0c69f2e16c3bc04e5f14",
     "url": "/pwa-robo-react-redax-app/static/css/main.dd26f316.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/pwa-robo-react-redax-app/static/css/1.f03d3d00.chunk.css"
   },
   {
-    "revision": "000a48e23304a8111548edcb07ad8726",
+    "revision": "7714671e019b7864509e5a20f8595d1b",
     "url": "/pwa-robo-react-redax-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/pwa-robo-react-redax-app/precache-manifest.a5864fbdc5936c36bc565ac260c5fa12.js"
+  "/pwa-robo-react-redax-app/precache-manifest.94d465e3c65f59321fd34c8b51bde243.js"
 );
 
 workbox.clientsClaim();
