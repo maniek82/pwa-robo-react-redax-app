b62850dfc5b3d09a2db2345f03a5041b78414629
maniek82 <mariusz.tripler@gmail.com>
2018-10-13 09:36:37 +0100

    Updates

---
+++ precache-manifest.46293f9f0d943cfe0e2b108e0dfe4e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50f9e9d8de9939ab5915",
+    "url": "/pwa-robo-react-app/static/js/runtime~main.50f9e9d8.js"
+  },
+  {
+    "revision": "1f4484565f0f8aeb3600",
+    "url": "/pwa-robo-react-app/static/js/main.1f448456.chunk.js"
+  },
+  {
+    "revision": "59c81c6d1c809d4b36dd",
+    "url": "/pwa-robo-react-app/static/js/1.59c81c6d.chunk.js"
+  },
+  {
+    "revision": "1f4484565f0f8aeb3600",
+    "url": "/pwa-robo-react-app/static/css/main.f8e7f7c5.chunk.css"
+  },
+  {
+    "revision": "59c81c6d1c809d4b36dd",
+    "url": "/pwa-robo-react-app/static/css/1.07994472.chunk.css"
+  },
+  {
+    "revision": "bee2aa2c3362f7da25bd529bf7ed5350",
+    "url": "/pwa-robo-react-app/index.html"
+  }
+];
